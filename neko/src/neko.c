0900e9da7fb96e850ab3f9a95a84ad5a89d67f77
nowotx <tux@xyecoc.com>
2026-02-12 16:10:05 +1000

    Fixed neko

--- neko/src/neko.c
+++ neko/src/neko.c
@@ -1,29 +1,29 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
-#include stdio.h
-#include fcntl.h
-#include unistd.h
-
-int main(int argc, char argv) {
+int main(int argc, char **argv) {
     int fd;
     char buf[4096];
     ssize_t n;
     
-    if (argc  2) {
-        while ((n = read(0, buf, sizeof(buf)))  0) {
+    if (argc < 2) {
+        while ((n = read(0, buf, sizeof(buf))) > 0) {
             write(1, buf, n);
         }
         return 0;
     }
     
-    for (int i = 1; i  argc; i++) {
+    for (int i = 1; i < argc; i++) {
         fd = open(argv[i], O_RDONLY);
-        if (fd  0) {
-            write(2, neko , 6);
+        if (fd < 0) {
+            write(2, "neko: ", 6);
             write(2, argv[i], strlen(argv[i]));
-            write(2,  No such filen, 16);
+            write(2, ": No such file\n", 16);
             continue;
         }
-        while ((n = read(fd, buf, sizeof(buf)))  0) {
+        while ((n = read(fd, buf, sizeof(buf))) > 0) {
             write(1, buf, n);
         }
         close(fd);
